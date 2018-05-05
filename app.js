b7cc66262c5c9aa7dd728466ccbeae66f16657fe
jaymccormick <jaymccormi@gmail.com>
2018-05-05 15:46:28 -0400

    listener on ul, ul add/remove handlers

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 let addInput = document.querySelector('input.add-item');
 const addButton = document.querySelector('button.add-item');
 const removeButton = document.querySelector('button.remove-item');
-const ul = document.querySelector('ul');
-const liList = document.getElementsByTagName('li');
 const divList = document.querySelector('.list');
+const listUL = divList.querySelector('ul');
+const liList = document.getElementsByTagName('li');
+
 
 addButton.addEventListener('click', () => {
   const text = addInput.value;
   const li = document.createElement('li');
   li.textContent = text;
-  ul.appendChild(li);
+  listUL.appendChild(li);
   addInput.value = '';
 });
 
 removeButton.addEventListener('click', () => {
   const lastLI = liList[liList.length - 1];
-  ul.removeChild(lastLI);
+  listUL.removeChild(lastLI);
 });
 
-/**
+
+// divList.addEventListener('click', (ev) => { });
 // works, but blows away the whole page when any button other than an li button
 // is clicked
-divList.addEventListener('click', (ev) => {
+listUL.addEventListener('click', (ev) => {
   // event is an object w/ attributes and methods
   // use event.target to get element where event started
   if (ev.target.tagName == 'BUTTON'){
@@ -30,4 +32,3 @@ divList.addEventListener('click', (ev) => {
     ul.removeChild(currentLI);
   }
 });
-*/
